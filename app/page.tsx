913c7b2c0048fbb8bf164a1ed7970fb8f742d779
Israel <israelmelorenbinongo@gmail.com>
2026-02-22 02:36:19 +0800

    chore: remove temporary style testing components

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <div className="flex items-center justify-center bg-green-dark w-48 h-48">
-          <div className="bg-surface-accent w-24 h-24"></div>
-      </div>
-     
 
     </div>
   );
