ebead1da595c76ccf7ef89003db8932c2c704cff
Israel <israelmelorenbinongo@gmail.com>
2026-03-03 16:10:35 +0800

    types: define initial navigation links type.

---
+++ types/navigation.ts
@@ -0,0 +1,20 @@
+export const NAV_CONFIG = {
+    viewer: [
+        { name: "Explore Feed", href: "/viewer/explore" },
+        { name: "My Following", href: "/viewer/following" },
+        { name: "Project Map", href: "/viewer/project-map" },
+        { name: "Support", href: "/viewer/support" },
+        { name: "Settings", href: "/viewer/settings" },
+    ],
+    'project-manager': [
+        { name: "Dashboard", href: "/project-manager/dashboard" },
+        { name: "Projects", href: "/project-manager/projects" },
+        { name: "Tasks", href: "/project-manager/tasks" },
+        { name: "Budget", href: "/project-manager/budget" },
+        { name: "Support", href: "/project-manager/support" },
+        { name: "Settings", href: "/project-manager/settings" },
+
+    ],
+    admin: [
+    ],
+}
